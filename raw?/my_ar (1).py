7770d088e493e5f155c43e7d9771a80591c331dc
Itamar Ginsberg <itamar.gins@gmail.com>
2022-02-13 12:09:10 +0200

    Add files via upload

---
+++ raw?/my_ar (1).py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import scipy
+from matplotlib import pyplot as plt
+import my_homography as mh
+#Add imports if needed:
+    """
+    Your code here
+    """
+#end imports
+
+#Add functions here:
+"""
+   Your code here
+"""
+#Functions end
+
+# HW functions:
+def create_ref(im_path):
+    """
+       Your code here
+    """
+    return ref_image
+
+if __name__ == '__main__':
+    print('my_ar')
